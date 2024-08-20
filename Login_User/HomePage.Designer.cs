1a09aaf22bfe363c5c805c93afa350fc8e6bd8c3
MickanoRakotomalala <rakotomalalamickano@gmail.com>
2024-08-20 16:01:13 +0300

    update HomePage

--- Login_User/HomePage.Designer.cs
+++ Login_User/HomePage.Designer.cs
@@ -33,6 +33,7 @@
             this.Menu = new System.Windows.Forms.ToolStripMenuItem();
             this.Session_Name = new System.Windows.Forms.ToolStripMenuItem();
             this.Logout = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.MenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,8 +43,8 @@
             this.Menu});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
-            this.MenuStrip.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.MenuStrip.Size = new System.Drawing.Size(1180, 40);
+            this.MenuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.MenuStrip.Size = new System.Drawing.Size(885, 40);
             this.MenuStrip.TabIndex = 11;
             this.MenuStrip.Text = "Menu";
             // 
@@ -68,7 +69,7 @@
             this.Session_Name.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Session_Name.Name = "Session_Name";
             this.Session_Name.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.Session_Name.Size = new System.Drawing.Size(210, 28);
+            this.Session_Name.Size = new System.Drawing.Size(151, 24);
             this.Session_Name.Text = "Session_Name";
             this.Session_Name.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.Session_Name.Click += new System.EventHandler(this.Session_Name_Click);
@@ -79,22 +80,32 @@
             this.Logout.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Logout.Image = global::Login_User.Properties.Resources.logout;
             this.Logout.Name = "Logout";
-            this.Logout.Size = new System.Drawing.Size(210, 28);
+            this.Logout.Size = new System.Drawing.Size(151, 24);
             this.Logout.Text = "Logout";
             this.Logout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Logout.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             this.Logout.Click += new System.EventHandler(this.Logout_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(140)))), ((int)(((byte)(172)))));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(243, 501);
+            this.panel1.TabIndex = 12;
+            // 
             // HomePage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1180, 617);
+            this.ClientSize = new System.Drawing.Size(885, 501);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.MenuStrip);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.MenuStrip;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "HomePage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HomePage";
@@ -112,5 +123,6 @@
         public System.Windows.Forms.ToolStripMenuItem Menu;
         public System.Windows.Forms.ToolStripMenuItem Session_Name;
         private System.Windows.Forms.ToolStripMenuItem Logout;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Login_User/Program.cs
+++ Login_User/Program.cs
@@ -21,7 +21,7 @@ namespace Login_User
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new HomePage());
         }
 
     }
